757a5f41227b5b4ad3fbaf58bb18fa387a9b5d11
JordaneBuena <121666719+JordaneBuena@users.noreply.github.com>
2023-02-22 13:51:50 +0100

    First commit back

---
+++ ScolaireRestApi/src/test/java/com/apprest/scolaire/ScolaireRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apprest.scolaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScolaireRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
